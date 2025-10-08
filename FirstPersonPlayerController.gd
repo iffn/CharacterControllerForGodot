bbbe851267c76414cdbe60f7a09c6fd39de97d5a
iffn <iffn1990@gmail.com>
2025-10-09 01:31:15 +0200

    Implemented sliding based on physics materials

--- FirstPersonPlayerController.gd
+++ FirstPersonPlayerController.gd
@@ -48,5 +48,52 @@ func _physics_process(delta):
 	
 	# Applying velocity
 	velocity = v
-
+	
+	## Sliding
+	
+	# Friction formulas:
+	# ------------------
+	# α = acos(Ny)
+	#   = atan(µ)
+	
+	# Ny = cos(α)
+  	#    = cos(atan(µ))
+		
+	# µ = tan(α)
+	#   = tan(acos(Ny)
+		
+	# α = slope angle
+	# Ny = Normalized normal y
+	# µ = Friction coefficient
+	
+	if is_on_floor():
+		var μ = get_floor_friction()
+		var max_angle = atan(μ)
+		floor_max_angle = max_angle
+	
 	move_and_slide()
+
+func get_floor_collision() -> KinematicCollision3D:
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_normal().dot(Vector3.UP) > sin(deg_to_rad(5.0)):
+			return collision
+	return null
+
+func get_floor_friction() -> float:
+	var col := get_floor_collision()
+	if col == null:
+		return 1.0
+
+	var collider := col.get_collider()
+	if collider is PhysicsBody3D and collider.physics_material_override:
+		return collider.physics_material_override.friction
+
+	# Check child CollisionShapes (in case material is defined there)
+	# Throws error...
+	#if collider is Node3D:
+		#for child in collider.get_children():
+			#if child is CollisionShape3D and child.physics_material_override:
+				#return child.physics_material_override.friction
+
+	return 1.0
