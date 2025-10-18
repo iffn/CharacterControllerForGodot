91d6ff1db50ceb54fbab315095c464832d42a087
iffn <iffn1990@gmail.com>
2025-10-18 10:50:23 +0200

    Started swim system switch
    Requires knowing if the player should be in the swim state: In water, not grounded, camera near or below water surface

--- Scripts/MovementModules/SwimDetector.gd
+++ Scripts/MovementModules/SwimDetector.gd
@@ -2,12 +2,17 @@ extends MovementModule
 
 class_name SwimDetector
 
+@export var _swim_system_to_switch_to : MovementSystem
+
+var in_water: bool:
+	get:
+		var in_areas := linked_player_controller.in_areas
+		for i in range(in_areas.size()):
+			var fluid := in_areas[i] as Fluid
+			if(fluid):
+				return true
+		return false
+
 func callable_physics_process(delta: float) -> void:
-	var in_areas := linked_player_controller.in_areas
-	
-	#print(in_areas.size())
-	
-	for i in range(in_areas.size()):
-		var fluid := in_areas[i] as Fluid
-		if(fluid):
-			print("swim")
+	if(_swim_system_to_switch_to && in_water):
+		linked_player_controller.current_movement_system = _swim_system_to_switch_to
