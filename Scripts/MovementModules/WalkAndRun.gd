46c252d26713edece5d4695016265668640af06c
iffn <iffn1990@gmail.com>
2025-10-23 09:19:56 +0200

    Cleaned up walk and run

--- Scripts/MovementModules/WalkAndRun.gd
+++ Scripts/MovementModules/WalkAndRun.gd
@@ -2,12 +2,12 @@ extends MovementModule
 class_name WalkAndRun
 
 func callable_physics_process(delta: float) -> void:
-	
-	var forward_speed: float
-	
+	# Get input
 	var input: Vector2 = linked_player_controller.input_move_direction_fwd_right
 	
-	# Only sprint when going forward,
+	# Determine forward speed. Only sprint when going forward,
+	var forward_speed: float
+	
 	if(input.x > 0):
 		forward_speed = lerp(
 			linked_player_controller.walk_speed,
@@ -17,11 +17,12 @@ func callable_physics_process(delta: float) -> void:
 	else:
 		forward_speed = linked_player_controller.walk_speed
 	
+	# Determine and apply new velocity.
 	var new_velocity: Vector2 = Vector2(
 		forward_speed,
 		linked_player_controller.walk_speed
 	)
 	
-	new_velocity *= linked_player_controller.input_move_direction_fwd_right
+	new_velocity *= input.normalized() # Prevent angeled walking being faster
 	
 	linked_player_controller.relative_horizontal_velocity_fwd_right = new_velocity
