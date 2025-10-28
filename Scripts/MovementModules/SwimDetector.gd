53ca272fbdb2061ba3df8009682c64bf4a9bd8f6
iffn <iffn1990@gmail.com>
2025-10-28 17:44:55 +0100

    Implemented switch back to normal movement

--- Scripts/MovementModules/SwimDetector.gd
+++ Scripts/MovementModules/SwimDetector.gd
@@ -3,6 +3,9 @@ extends MovementModule
 class_name SwimDetector
 
 @export var _swim_system_to_switch_to : MovementSystem
+@export var _movement_system_to_switch_to : MovementSystem
+
+var _swimming := false
 
 func in_fluid(sample_point_world: Vector3) -> bool:
 	var in_areas := linked_player_controller.in_areas
@@ -14,6 +17,17 @@ func in_fluid(sample_point_world: Vector3) -> bool:
 	return false
 
 func callable_physics_process(delta: float) -> void:
-	if(_swim_system_to_switch_to):
-		if(in_fluid(linked_player_controller.head_position_world)):
-			linked_player_controller.current_movement_system = _swim_system_to_switch_to
+	
+	if(_swimming):
+		if(_movement_system_to_switch_to):
+			if(!in_fluid(linked_player_controller.head_position_world)
+				&& linked_player_controller.is_on_floor()):
+				print("Swiching to move")
+				linked_player_controller.current_movement_system = _movement_system_to_switch_to
+				_swimming = false
+	else:
+		if(_swim_system_to_switch_to):
+			if(in_fluid(linked_player_controller.head_position_world)):
+				print("Swiching to swim")
+				linked_player_controller.current_movement_system = _swim_system_to_switch_to
+				_swimming = true
